e170a059494aa3faaf8b76a68ed97d029ecf8233
aerickson <andrew.erickson@riverbed.com>
2017-01-06 14:03:08 -0500

    man said clean him up, clean him up...

--- main.go
+++ main.go
@@ -1,46 +1,13 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	// nice stuff, https://github.com/gin-gonic/gin
 	"github.com/andrewerickson/golearn/routes"
 	"gopkg.in/gin-gonic/gin.v1"
-	"html/template"
-	"log"
-	"math/rand"
-	"net/http"
-	// "time"
 )
 
-func add(x int, y int) int {
-	return x + y
-}
-
-var listenAddr = flag.String("addr", "127.0.0.1:1718", "http service address") // Q=17, R=18
-
-func myHandler(w http.ResponseWriter, req *http.Request) {
-	log.Output(1, fmt.Sprintf("processing request with form value %v", req.FormValue("s")))
-	templ.Execute(w, req.FormValue("s"))
-}
-
-var templ = template.Must(template.New("qr").Parse(templateStr))
-
 func main() {
-	fmt.Println("Hello, 世界")
-	fmt.Println("my fav number is", rand.Intn(10))
-	fmt.Println("lets add some stuff", add(rand.Intn(12), rand.Intn(11)))
 
-	flag.Parse()
-	/* server := &http.Server{
-		Addr:           *listenAddr,
-		Handler:        http.HandlerFunc(myHandler),
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	log.Fatal(server.ListenAndServe())
-	*/
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
@@ -53,16 +20,3 @@ func main() {
 	})
 	router.Run()
 }
-
-const templateStr = `
-<html>
-<head>
-<title>Come at me bro</title>
-</head>
-<body>
-<br>
-Gin n' joiuce!
-<br>
-</body>
-</html>
-`
